401a5e51c4ff42ed4984676517fdf610dff2363e
MuhammedRk <70426191+MuhammedRk@users.noreply.github.com>
2023-04-20 12:15:36 +0530

    Add files via upload

---
+++ plugins/javascript/service-icon.js
@@ -0,0 +1,14 @@
+function openhelpline() { document.getElementById("helplineservicedata").style.width = "100%"; }
+function closehelplinedata() { document.getElementById("helplineservicedata").style.width = "0%"; }
+
+function openfastdelivery() { document.getElementById("fastdelivaryservicedata").style.width = "100%"; }
+function closefastdelivarydata() { document.getElementById("fastdelivaryservicedata").style.width = "0%"; }
+
+function openbuy() { document.getElementById("buyservicedata").style.width = "100%"; }
+function closebuydata() { document.getElementById("buyservicedata").style.width = "0%"; }
+
+function openlocation() { document.getElementById("locstionservicedata").style.width = "100%"; }
+function closelocstiondata() { document.getElementById("locstionservicedata").style.width = "0%"; }
+
+function openinstagram() { document.getElementById("instagramservicedata").style.width = "100%"; }
+function closeinstagram() { document.getElementById("instagramservicedata").style.width = "0%"; }
